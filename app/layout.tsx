a32e52eb0ae48156f96f35329ce0bf6dcbd4faa4
wade-loccisano <wade.loccisano@gmail.com>
2024-07-09 23:14:04 -0400

    add splash component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,3 +28,101 @@ export default function RootLayout({
     </html>
   );
 }
+
+// # Wade Loccisano
+
+// wade.loccisano@gmail.com | 724-494-4379 | [linkedin.com/in/wade-loccisano](https://linkedin.com/in/wade-loccisano)
+
+// <!-- ## About -->
+// <!-- change to summary of experience -->
+// Dynamic and results-oriented Software Developer with experience in delivering high-quality web apps for clients and customers on a global scale. Skilled in full-stack development, Agile methodologies, and continuous integration practices. Seeking to leverage expertise in software engineering to contribute to innovative projects in a highly collaborative and ever-evolving team environment.
+
+// ## Skills
+
+// ### Front-end
+// - HTML/CSS
+// - JavaScript (ES6, React, Redux, Node.js, Next.js, JSX)
+// - TypeScript (Next.js, TSX, Angular)
+
+// ### Back-end
+// - C# (.NET)
+// - Python (Flask, Pandas)
+// - ORM (Entity Framework, Prisma)
+// - SQL (Postgresql)
+
+// ### DevOps
+// - Cloud (AWS)
+// - Version Control and CI/CD (Git, Docker)
+// - Experienced with Agile development methodologies
+
+// ## Experience
+
+// ### **Full-stack Software Developer** | MadeLabs LLC. | Remote, United States | *April 2021 - March 2024*
+
+// #### Website Development
+// <!-- FrontEnd -->
+
+// **Technologies:** JavaScript, Next.js, React.js, Docker, AWS Elastic Container Service, Tailwind CSS, Prisma ORM
+
+// - Led the development and maintenance efforts for the MadeLabs company website, leveraging Next.js and React.js frameworks to deliver high-performance web applications and reusable components, increasing Lighthouse performance metrics by 20 basis points.
+// - Optimized Docker configurations and facilitated deployments to the AWS Elastic Container Service, ensuring scalability and global accessibility of the website.
+// - Tailored user-facing components to meet design and theming requirements, utilizing CSS and the Tailwind CSS framework to enhance visual appeal and usability.
+
+// #### Point of Sale Data Integration
+// <!-- DevOps -->
+
+// **Technologies:** Python, AWS S3, AWS Step Functions, AWS Lambda, AWS Redshift
+
+// - Spearheaded the end-to-end data migration and transformation process, leveraging AWS services including S3, Lambda, and Step Functions, to seamlessly integrate millions of point-of-sale data entries into centralized repositories.
+// - Designed and implemented data pipelines to extract, transform, and load (ETL) raw data into Amazon Redshift, ensuring accuracy and consistency.
+// - Developed custom Lambda functions to automate data processing tasks, optimizing efficiency and reducing manual intervention.
+// - Implemented rule-based configurations and S3 triggers to orchestrate automated workflows, enabling real-time data updates and alerts.
+// - Collaborated closely with cross-functional teams to translate business requirements into actionable data solutions, resulting in enhanced visibility and insights into sales performance.
+
+// #### Fairview Project Management Tool
+// <!-- FrontEnd, BackEnd, DevOps -->
+
+// **Technologies:** JavaScript, React.js, .NET, Entity Framework, Amazon Web Services
+
+// - Worked on the development of the Fairview Project Management Tool at MadeLabs, orchestrating the implementation of project management interfaces and dashboards.
+// - Designed and engineered intuitive user interfaces while leveraging npm packages for enhanced functionality, efficiency, and ease of use.
+// - Provided a critical link between technology and user experience, ensuring seamless navigation and reporting for project stakeholders.
+
+// #### ATP/Flightdocs Tracking
+// <!-- FrontEnd, BackEnd, DevOps -->
+
+// **Technologies:** TypeScript, Angular, .NET, Entity Framework, Amazon Web Services
+
+// - Directed the integration and synchronization of data across customer identity, maintenance requests, and parts inventory systems, optimizing operational efficiency and data consistency.
+// - Leveraged Swagger to generate REST APIs, facilitating seamless communication and interoperability between disparate systems and services.
+// - Utilized a robust tech stack comprising TypeScript, Angular, and AngularJS, .NET to develop feature-rich, scalable applications that met the dynamic needs of the aviation industry.
+// - Played a pivotal role in enhancing user authorization, authentication, and permissions, providing customers and administrators with the functionality to make informed decisions and drive operational excellence.
+
+
+// ## Certifications
+
+// AWS Certified Cloud Practitioner
+// <!-- AWS Certified Solutions Architect - Associate -->
+
+// ## Projects
+
+// ### Resume Site
+
+// Developed a personal website to host my resume using Next.js.
+// Added Docker containerization to allow easy collaboration and deployment across a variety of systems and services.
+// Leveraged this site as a learning opportunity to improve my UI/UX, CI/CD, and project management skillsets.
+
+// ## Misc
+
+// ### Hobbies
+
+// Coding
+// Miniature Painting
+// Walking the dog
+
+// ### Education:
+
+// Web Application Development Bootcamp
+// Tech Elevator, National Live Remote, United States
+// Graduated: February 2021
+
